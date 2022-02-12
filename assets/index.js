c8e37ccdf424475e3f5301eeeae99f3a4b54c2cc
Jimer Samuel Espinoza <ejimersamo@gmail.com>
2022-02-12 11:45:06 -0500

    Version 1.0.2

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,22 @@
-function MediaPlayer(config) {
-    this.media = config.el;
-}
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
-MediaPlayer.prototype.play = function() {
-   this.media.play();
-};
+const video = document.querySelector("video");
 
-MediaPlayer.prototype.pause = function() {
-   this.media.pause();
-};
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
 
-MediaPlayer.prototype.togglePlay = function() {
-   if(this.media.paused) {
-       this.play();
-   } else {
-       this.pause();
-   }
-};
-
-const video = document.querySelector('video');
-const player = new MediaPlayer( { el: video});
-
-const button = document.querySelector('button');
+const button = document.querySelector("button");
 button.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector("#muteButton");
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
